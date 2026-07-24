b48a21b2c50afec3fbad822c721285a3bdf7e9e5
Mars <210487034+marsDSP@users.noreply.github.com>
2026-07-24 11:48:00 -0500

    Remove redundant comments and clean up `ChronosEditor`

--- source/ChronosEditor.cpp
+++ source/ChronosEditor.cpp
@@ -2,8 +2,7 @@
 #include "ChronosEditor.h"
 
 //==============================================================================
-ChronosEditor::ChronosEditor (ChronosProcessor& p)
-    : AudioProcessorEditor (&p), pref (p)
+ChronosEditor::ChronosEditor (ChronosProcessor& p) : AudioProcessorEditor (&p), pref (p)
 {
     ignoreUnused (pref);
     // Make sure that before the constructor has finished, you've set the
@@ -18,12 +17,6 @@ ChronosEditor::~ChronosEditor()
 //==============================================================================
 void ChronosEditor::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setColour (Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void ChronosEditor::resized()
--- source/ChronosEditor.h
+++ source/ChronosEditor.h
@@ -14,8 +14,6 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
     ChronosProcessor& pref;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ChronosEditor)
